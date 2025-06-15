43d94b1b132d43530831c1514ee19c1f21205fbc
steevenmendozau <steeven.mendoza@uees.edu.ec>
2025-06-15 09:45:09 -0500

    Creacion del componente Home

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
