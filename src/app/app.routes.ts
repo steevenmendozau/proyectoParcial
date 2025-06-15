080de1c1f993683d145c9e69a986ae73069b174d
steevenmendozau <steeven.mendoza@uees.edu.ec>
2025-06-15 11:33:59 -0500

    Configuracion de las Rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { Gastronomia } from './pages/gastronomia/gastronomia';
 import { Festividades } from './pages/festividades/festividades';
 
 export const routes: Routes = [
+    { path: '', component: Home },
     { path: 'inicio', component: Home },
     { path: 'regiones', component: Regiones },
     { path: 'gastronomia', component: Gastronomia },
