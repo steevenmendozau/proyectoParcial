446d968dae1bbb4ca44d22881ddaa526641eb558
steevenmendozau <steeven.mendoza@uees.edu.ec>
2025-06-14 20:24:52 -0500

    Creacion del Componente Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./shared/header/header";
+import { Menu } from "./shared/menu/menu";
+import { Footer } from './shared/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Menu, Footer],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
